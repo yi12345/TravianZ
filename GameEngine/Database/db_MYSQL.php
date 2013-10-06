f7b44fd931c3c06c7612d0250663ac778f00eab7
Shadowss <cata7007@gmail.com>
2013-10-06 11:17:16 +0300

    FIX NAME OF VILLAGE hack , script alert

--- GameEngine/Database/db_MYSQL.php
+++ GameEngine/Database/db_MYSQL.php
@@ -1555,7 +1555,8 @@ class MYSQL_DB {
 	}
 
 	function setVillageName($vid, $name) {
-		if(!empty($name))
+		$name = trim($name);
+		if((!empty($name)) && ($name == htmlspecialchars($name)))
 		{
 		$q = "UPDATE " . TB_PREFIX . "vdata set name = '$name' where wref = $vid";
 		return mysql_query($q, $this->connection);
