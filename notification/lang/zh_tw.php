3689cc6809327b468ff176d4307d29fa0c5a6c0a
yoyo930021 <yoyo930021@gmail.com>
2014-12-06 00:26:57 +0800

    404標題繁化 notification繁化 內容稀少嘗試看看www

---
+++ notification/lang/zh_tw.php
@@ -0,0 +1,43 @@
+<?php
+#################################################################################
+##                                                                             ##
+##              -= YOU MUST NOT REMOVE OR CHANGE THIS NOTICE =-                ##
+##                                                                             ##
+## --------------------------------------------------------------------------- ##
+##                                                                             ##
+##  Project:       ZravianX & TravianZ                                         ##
+##  Version:       2011.11.02                                                  ##
+##  Filename:      notification/lang/en.php                                    ##
+##  Developed by:  ZZJHONS & Songeriux                                         ##
+##  License:       Creative Commons BY-NC-SA 3.0                               ##
+##  Copyright:     ZravianX (c) 2011 - All rights reserved                     ##
+##  URLs:          http://zravianx.zzjhons.com                                 ##
+##  Source code:   http://www.github.com/songeriux/TravianZ                    ##
+##                                                                             ##
+#################################################################################
+
+$lang['notification']['awaits_you'] = "歡迎光臨!";
+$lang['notification']['join_now'] = "現在就加入戰局來證明自己的實力!";
+$lang['notification']['new_challenges'] = "一場全新的挑戰，令人驚嘆的圖形和許多全新的功能等待你。成為第一批玩家，開始爭奪對數以百萬計村莊，收集黃金和增加你的人口。";
+$lang['notification']['notify_me'] = "請通知我，當TRAVIAN4開始時";
+$lang['notification']['email'] = "Email";
+$lang['notification']['send'] = "送出";
+$lang['notification']['error'] = "對不起，這裡伺服器無法工作.";
+$lang['notification']['i_agree'] = "我同意TRAVIAN可以告訴我有趣的新聞和遊戲相關的信息。";
+$lang['notification']['facebook_page'] = "http%3A%2F%2Fwww.facebook.com%2Ftravianx";
+$lang['notification']['see_features'] = "察看新功能在TRAVIAN 4";
+$lang['notification']['screenshots'] = "遊戲畫面";
+$lang['notification']['new_design'] = "一種全新的設計";
+$lang['notification']['new_graphics'] = "TRAVIAN4製作了全新細緻的圖形，不僅體現遊戲的進步，也傳遞有價值的信息給玩家。";
+$lang['notification']['more'] = "更詳細的信息可以在開發的部落格上找到。";
+$lang['notification']['title1'] = "農業全新的設計";
+$lang['notification']['desc1'] = "TRAVIAN4製作了全新細緻的圖形，不僅體現遊戲的進步，也傳遞有價值的信息給玩家。";
+$lang['notification']['title2'] = "改進的英雄系統";
+$lang['notification']['desc2'] = "先進的英雄系統，數百個的挑戰和冒險等待著你。";
+$lang['notification']['title3'] = "地圖縮放";
+$lang['notification']['desc3'] = "全新的地圖包含幾個非常有用的功能，這將為玩家提供比以往更多的資訊。從來沒有像現在這麼規劃你的下一個步驟。";
+$lang['notification']['title4'] = "一個新的部落";
+$lang['notification']['desc4'] = "由達賴族部落所控制的村莊將捍衛自己的區域。但是，一旦戰敗，這些村莊可以用來增加你的收入。";
+$lang['notification']['titel5'] = "許多改進";
+$lang['notification']['desc5'] = "許多改進TRAVIAN4都基於我們的玩家所做的建議。包括：增強的導航，直接進入網頁和論壇等。";
+?>
